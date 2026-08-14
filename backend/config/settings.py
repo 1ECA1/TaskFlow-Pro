4db5851f401acd135e897101285eecaf2f623df9
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2026-08-14 20:38:45 +0100

    Fix allowed hosts for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-7km0oe+sy1b6!)+sirdt*@!fvponw0obqmgq6ub=*d)loxvcf@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "[::1]",
+    "taskflow-pro-hurb.onrender.com",
+]
 
 
 # Application definition
